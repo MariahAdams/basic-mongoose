67f2bcd83c32a6e1e91d41dc84741a59b7525df9
MariahAdams <mariah.adams23@yahoo.com>
2018-07-24 19:37:24 -0700

    hello GET complete

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const Hapi = require('hapi');
+const Mongoose = require('mongoose');
+const Movie = require('./lib/models/movie');
+
+Mongoose.connect('mongodb://localhost:27017/testing');
+Mongoose.connection.once('open', () => {
+    console.log('connected to MongoDB database');
+});
 
 const server = Hapi.Server({ 
     port: '8000',
     host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(req, reply) {
+        return reply.response('Hello HapiJS!');
+    }
 });
 
 const init = async() => {
