db3ae2c77eef1e0b2efa45349c9289d6c955f628
MariahAdams <mariah.adams23@yahoo.com>
2018-07-25 08:52:00 -0700

    saving progress

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Hapi = require('hapi');
 const Mongoose = require('mongoose');
 const Movie = require('./lib/models/movie');
 
-Mongoose.connect('mongodb://localhost:27017/testing');
+Mongoose.connect('mongodb://localhost:27017/ghibli');
 Mongoose.connection.once('open', () => {
     console.log('connected to MongoDB database');
 });
@@ -12,13 +12,28 @@ const server = Hapi.Server({
     host: 'localhost'
 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(req, reply) {
-        return reply.response('Hello HapiJS!');
+server.route(
+    {
+        method: 'GET',
+        path: '/',
+        handler: function(req, reply) {
+            return reply.response('Hello HapiJS!');
+        }
+    },
+    {
+        method: 'GET',
+        path: '/api/movies',
+        handler: function(req, res) {
+            return Movie.find({});
+            // Movie.find(req.query)
+            // .select('name')
+            // .lean()
+            // .then(movies => {
+            //     reply.res.json(movies);
+            // });
+        }
     }
-});
+);
 
 const init = async() => {
     
